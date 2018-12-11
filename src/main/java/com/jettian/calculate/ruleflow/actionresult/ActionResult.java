d7a33427332a7c19e3ea4c7ee34e00f2fa6aafa0
田杰 <tian.jie@rongzer.com>
2018-12-11 13:16:17 +0800

    新增顶层接口default 方法

--- src/main/java/com/jettian/calculate/ruleflow/actionresult/ActionResult.java
+++ src/main/java/com/jettian/calculate/ruleflow/actionresult/ActionResult.java
@@ -1,8 +1,32 @@
 package com.jettian.calculate.ruleflow.actionresult;
 
+import com.jettian.calculate.ruleflow.anno.KnowledgeNameAndSource;
+import com.jettian.calculate.ruleflow.anno.RuleResult;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 接受结果顶层接口
  * @author tianjie
  */
 public interface ActionResult {
+
+    /**
+     * 获取结果类别
+     * @return
+     */
+    default List<String> getKnowledges(){
+        List<String> actionResults = new ArrayList<>();
+        Field[] fields = this.getClass().getDeclaredFields();
+        for(Field field : fields){
+            RuleResult ruleResult = field.getAnnotation(RuleResult.class);
+            if(null!=ruleResult){
+                actionResults.add(ruleResult.value());
+            }
+        }
+
+        return actionResults;
+    }
 }
--- src/main/java/com/jettian/calculate/ruleflow/knowledge/Knowledge.java
+++ src/main/java/com/jettian/calculate/ruleflow/knowledge/Knowledge.java
@@ -1,8 +1,31 @@
 package com.jettian.calculate.ruleflow.knowledge;
 
+import com.jettian.calculate.ruleflow.anno.KnowledgeNameAndSource;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 知识顶层接口
  * @author tianjie
  */
 public interface Knowledge {
+
+    /**
+     * 获取知识类别
+     * @return
+     */
+    default List<String> getKnowledges(){
+        List<String> knowledges = new ArrayList<>();
+        Field[] fields = this.getClass().getDeclaredFields();
+        for(Field field : fields){
+            KnowledgeNameAndSource knowledgeNameAndSource = field.getAnnotation(KnowledgeNameAndSource.class);
+            if(null!=knowledgeNameAndSource){
+                knowledges.add(knowledgeNameAndSource.name());
+            }
+        }
+
+        return knowledges;
+    }
 }
